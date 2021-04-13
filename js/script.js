2b00c60db830275f4098d9c8b33c0611d060a698
Adrian <32913236+adrigski@users.noreply.github.com>
2021-04-12 21:31:28 -0500

    added function to alert button, input text, and updating HTML text

--- js/script.js
+++ js/script.js
@@ -1,11 +1,17 @@
 function init(){
 //add your javascrip between these two lines of code
- 
+    
+    var alertMeButton = document.getElementById("entrybutton");
 
-
-
-
-
-
-
+    function alertMe() {
+        var inText = document.getElementById("entryinput");
+        var outText = document.getElementById("textoutput");
+        alert("Adrian Grabowski: " + inText.value);
+        outText.innerHTML = inText.value;
+    }
+    
+    alertMeButton.addEventListener('click', alertMe);
+}
 window.addEventListener('load', init);
+
+
