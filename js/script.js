93e22d73afc20e9112fbace9029f78ee5d11ee7b
Adrian <32913236+adrigski@users.noreply.github.com>
2021-04-12 21:34:43 -0500

    fixed typo

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 function init(){
-//add your javascrip between these two lines of code
+//add your javascript between these two lines of code
     
     var alertMeButton = document.getElementById("entrybutton");
 
@@ -9,7 +9,6 @@ function init(){
         alert("Adrian Grabowski: " + inText.value);
         outText.innerHTML = inText.value;
     }
-    
     alertMeButton.addEventListener('click', alertMe);
 }
 window.addEventListener('load', init);
